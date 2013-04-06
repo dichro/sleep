d442cf4319f1e97ea7fc0c9569b733bccc7afec5
Mikolaj Habryn <dichro@rcpt.to>
2013-04-06 18:27:38 +0000

    Unimodal normal estimator.

--- sample.py
+++ sample.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from ZeoRawData.BaseLink import BaseLink
 from ZeoRawData.Parser import Parser
 
+import math
 import time
 
 port = '/dev/ttyUSB0'
@@ -12,24 +13,53 @@ param = 'SleepStage'
 
 bins = ['11-14', '2-4', '4-8', '8-13', '13-18', '18-21', '30-50']
 
+class Estimator:
+	'''Normal estimator, completely wrong: should be bimodal Poisson.'''
+	count = 0
+	sum = 0.0
+	sumsq = 0.0
+	
+	def mean(self):
+		return self.sum / self.count
+	
+	def sigma(self):
+		mean = self.mean()
+		variance = (self.sumsq - 2*mean*self.sum + self.count*mean*mean)/(self.count - 1)
+		return math.sqrt(variance)
+
+	def p(self, value):
+		try:
+			return (value - self.mean()) / self.sigma()
+		except ZeroDivisionError, e:
+			return None
+
+	def add(self, value):
+		p = self.p(value)
+		self.count += 1
+		self.sum += value
+		self.sumsq += value*value
+		return p
+
+
 class Act:
-	tracking = True
+	estimator = Estimator()
 
 	def onSlice(self, slice):
-		if not self.tracking:
+		if self.estimator is None:
 			return
 		try:
 			gamma = slice['FrequencyBins']['30-50']
+			p = self.estimator.add(gamma)
+			print(gamma, p)
 		except KeyError, e:
 			print(e, slice)
-		print(gamma)
 
 	def onEvent(self, logtime, version, event):
 		if event == 'HeadbandUndocked':
-			self.tracking = True
+			self.estimator = Estimator()
 		if event == 'HeadbandDocked':
 			# TODO(dichro): reset
-			self.tracking = False
+			self.estimator = None
 
 
 class Log:
