36f8e7aa9c78cfc5338a80aef6d0a66bdf012981
Mikolaj Habryn <dichro@rcpt.to>
2013-04-07 20:06:32 +0000

    Remove estimator; add hack. Make sample.py importable.

--- sample.py
+++ sample.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 from ZeoRawData.BaseLink import BaseLink
 from ZeoRawData.Parser import Parser
+from onkyo import Receiver
 
 import math
 import time
@@ -13,54 +14,47 @@ param = 'SleepStage'
 
 bins = ['11-14', '2-4', '4-8', '8-13', '13-18', '18-21', '30-50']
 
-class Estimator:
-	'''Normal estimator, completely wrong: should be bimodal Poisson.'''
-	count = 0
-	sum = 0.0
-	sumsq = 0.0
-	
-	def mean(self):
-		return self.sum / self.count
-	
-	def sigma(self):
-		mean = self.mean()
-		variance = (self.sumsq - 2*mean*self.sum + self.count*mean*mean)/(self.count - 1)
-		return math.sqrt(variance)
-
-	def p(self, value):
-		try:
-			return (value - self.mean()) / self.sigma()
-		except ZeroDivisionError, e:
-			return None
-
-	def add(self, value):
-		p = self.p(value)
-		self.count += 1
-		self.sum += value
-		self.sumsq += value*value
-		return p
-
 
 class Act:
-	estimator = Estimator()
+	gammaThreshold = 0.002
+
+	def __init__(self):
+		self.reset(True)
 
 	def onSlice(self, slice):
-		if self.estimator is None:
+		if not self.sampling:
 			return
-		try:
-			gamma = slice['FrequencyBins']['30-50']
-			p = self.estimator.add(gamma)
-			print(gamma, p)
-		except KeyError, e:
-			print(e, slice)
+		if not slice['BadSignal']:
+			if slice['FrequencyBins']['30-50'] < self.gammaThreshold:
+				self.awakeSeconds = 0
+				self.asleepSeconds += 1
+			else:
+				self.awakeSeconds += 1
+				self.asleepSeconds = 0
+		if self.asleepSeconds > 30:
+			self.sleptSeconds += 1
+		if self.awakeSeconds > 120 and self.sleptSeconds > 6 * 3600:
+			self.wakeUp()
 
 	def onEvent(self, logtime, version, event):
 		if event == 'HeadbandUndocked':
-			self.estimator = Estimator()
+			self.reset(True)
 		if event == 'HeadbandDocked':
 			# TODO(dichro): reset
-			self.estimator = None
-
+			self.reset(False)
+	
+	def reset(self, sampling):
+		self.sampling = sampling
+		self.awakeSeconds = 0
+		self.sleepingSeconds = 0
+		self.sleptSeconds = 0
+	
+	def wakeUp(self):
+		'''Turns on lights and plays music.'''
+		print(time.ctime(), 'waking up after', self.sleptSeconds,
+			'sleep; awake for', self.awakeSeconds)
+		# urllib?
+		Receiver().start()
 
 class Log:
 	def __init__(self, filebase):
@@ -73,19 +67,20 @@ class Log:
 		print(time.ctime(), event, file=self.file)
 
 
-parser = Parser()
+if __name__ == '__main__':
+	parser = Parser()
 
-log = Log('/tmp/zeo')
-parser.addSliceCallback(log.onSlice)
-parser.addEventCallback(log.onEvent)
+	log = Log('/tmp/zeo')
+	parser.addSliceCallback(log.onSlice)
+	parser.addEventCallback(log.onEvent)
 
-act = Act()
-parser.addSliceCallback(act.onSlice)
-parser.addEventCallback(act.onEvent)
+	act = Act()
+	parser.addSliceCallback(act.onSlice)
+	parser.addEventCallback(act.onEvent)
 
-link = BaseLink(port)
-link.addCallback(parser.update)
-link.start()
+	link = BaseLink(port)
+	link.addCallback(parser.update)
+	link.start()
 
-while(True):
-	time.sleep(60)
+	while(True):
+		time.sleep(60)
